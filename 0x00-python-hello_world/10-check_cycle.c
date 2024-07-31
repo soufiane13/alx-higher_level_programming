8031da7fc8109db135dac8448d7c437f21e6cef1
soufiane benhabboul <soufiane0bn@gmail.com>
2024-07-30 17:05:54 -0700

    task ready to review

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_listint - prints all elements of a listint_t list
+ * @h: pointer to head of list
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *cur;
+	unsigned int n;
+
+	cur = h;
+	n = 0;
+	while (cur != NULL)
+	{
+		printf("%i\n", cur->n);
+		cur = cur->next;
+		n++;
+	}
+	return (n);
+}
+
+/**
+ * add_nodeint - adds a new node at the beginning of a listint_t list
+ * @head: pointer to a pointer of the start of the list
+ * @n: integer 
+ * Return: address of the new element or NULL 
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: pointer to list 
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *cur;
+
+	while (head != NULL)
+	{
+		cur = head;
+		head = head->next;
+		free(cur);
+	}
+}
