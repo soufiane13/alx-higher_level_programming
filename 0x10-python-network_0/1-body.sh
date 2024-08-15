cccda263e4e786678d3dec164785283f4e45ad8c
soufiane benhabboul <soufiane0bn@gmail.com>
2024-08-14 17:23:52 -0700

    first commit

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#  It Gets the response body for a given URL for 200 status code responses.
+curl -sL "$1"
---
+++ 0x10-python-network_0/2-delete.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# It Sends a DELETE request to a given URL and display the response body.
+curl -sX DELETE "$1"
