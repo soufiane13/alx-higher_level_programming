a3dea7497616baeda856527078f370385b0d9e22
soufiane benhabboul <soufiane0bn@gmail.com>
2024-08-15 18:35:09 -0700

    len to length

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -6,7 +6,7 @@
  */
 void print_python_string(PyObject *p)
 {
-	long int len;
+	long int length;
 
 	fflush(stdout);
 
@@ -17,12 +17,12 @@ void print_python_string(PyObject *p)
 		return;
 	}
 
-	len = ((PyASCIIObject *)(p))->len;
+	length = ((PyASCIIObject *)(p))->length;
 
 	if (PyUnicode_IS_COMPACT_ASCII(p))
 		printf("  type: compact ascii\n");
 	else
 		printf("  type: compact unicode object\n");
-	printf("  length: %ld\n", len);
-	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &len));
+	printf("  length: %ld\n", length);
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
 }
