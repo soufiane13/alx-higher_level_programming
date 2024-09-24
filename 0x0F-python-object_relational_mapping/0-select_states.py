2ca4fa7ac796113523a5d829cba0015653d3b920
soufiane benhabboul <soufiane0bn@gmail.com>
2024-09-24 04:32:37 -0700

    module is documented

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,6 +7,14 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
+    """
+    Module that lists all states from the database hbtn_0e_0_usa.
+    
+    Usage:
+        ./0-select_states.py <mysql username> \
+                             <mysql password> \
+                             <database name>
+    """
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM `states`")
